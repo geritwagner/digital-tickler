774a8ca67b0f54e600116884875359ed2fc36dee
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2020-08-01 08:54:41 +0200

    fix path

--- start_tickler.sh
+++ start_tickler.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gnome-terminal --geometry 80x25+100+100 -e "python3 .../tickler.py"
+gnome-terminal --geometry 80x25+100+100 -e "python3 /home/gerit/ownCloud/data/technology/workstation/scripts/digital-tickler/tickler.py"
 read input_variable
 echo " "
